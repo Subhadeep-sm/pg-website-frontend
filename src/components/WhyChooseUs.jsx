f0bf0497eec010f0f5c648141f52bc72f0b5a5df
Deep Samanta <deepsamanta804@gmail.com>
2025-07-24 05:19:57 +0530

    colors updated

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -6,7 +6,7 @@ import room4 from "../assets/room.jpg";
 
 const WhyChooseUs = () => {
   return (
-    <section className="bg-[#f0f9fb] px-6 py-16 text-[#152b37]">
+    <section className="bg-[#fff0db] px-6 py-16 text-[#222831]">
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-col md:flex-row items-center justify-between gap-12">
           
@@ -19,7 +19,7 @@ const WhyChooseUs = () => {
             
             {/* Center Circle */}
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
-              <div className="w-36 h-36 bg-[#295061] text-white flex items-center justify-center text-center text-lg font-bold rounded-full shadow-xl">
+              <div className="w-36 h-36 bg-[#948979] text-white flex items-center justify-center text-center text-lg font-bold rounded-full shadow-xl">
                 Why <br /> Choose <br /> Us
               </div>
             </div>
@@ -27,25 +27,25 @@ const WhyChooseUs = () => {
 
           {/* FEATURES LIST */}
           <div className="flex-1 space-y-4 text-white">
-            <div className="bg-[#295061] p-4 rounded shadow-md text-white">
+            <div className="bg-[#948979] p-4 rounded shadow-md text-white">
               <strong>Prime Location</strong> – Near top educational institutes & IT hub
             </div>
-            <div className="bg-[#295061] p-4 rounded shadow-md">
+            <div className="bg-[#948979] p-4 rounded shadow-md">
               <strong>Tasty & Healthy Meals</strong> – Daily home-cooked Veg and Non-Veg food
             </div>
-            <div className="bg-[#295061] p-4 rounded shadow-md">
+            <div className="bg-[#948979] p-4 rounded shadow-md">
               <strong>Safe & Secure Stay</strong> – 24/7 security & CCTV surveillance
             </div>
-            <div className="bg-[#295061] p-4 rounded shadow-md">
+            <div className="bg-[#948979] p-4 rounded shadow-md">
               <strong>Daily Cleaning</strong> – Hygiene is our top priority
             </div>
-            <div className="bg-[#295061] p-4 rounded shadow-md">
+            <div className="bg-[#948979] p-4 rounded shadow-md">
               <strong>Self Laundry</strong> – Each floor with washing machine
             </div>
-            <div className="bg-[#295061] p-4 rounded shadow-md">
+            <div className="bg-[#948979] p-4 rounded shadow-md">
               <strong>WiFi Available</strong> – 24/7 high-speed internet service
             </div>
-            <div className="bg-[#295061] p-4 rounded shadow-md">
+            <div className="bg-[#948979] p-4 rounded shadow-md">
               <strong>Affordable Pricing</strong> – Rooms starting at ₹6000 only
             </div>
           </div>
